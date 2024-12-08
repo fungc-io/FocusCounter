75bf43a7f2dc6dfc463bd796ec3b32837bf4faec
fungc-io <fung@oursky.com>
2024-12-08 22:56:00 +0000

    Initial Commit

---
+++ TallyTests/TallyTests.swift
@@ -0,0 +1,17 @@
+//
+//  TallyTests.swift
+//  TallyTests
+//
+//  Created by Fung on 08/12/2024.
+//
+
+import Testing
+@testable import Tally
+
+struct TallyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
